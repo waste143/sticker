80f883b10cba725f2f8f6f040298a51f4a1c75d7
p00ria <amir1300sobatdar@gmail.com>
2016-03-15 18:25:17 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = ""
+local bot_api_key = "141658524:AAECe5TNLvkoP4epppTiK4LMDhgii7NgyIs"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
@@ -29,7 +29,7 @@ source : [click here](https://Github.com/Iamjavid/sticker_bot)
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {}-- put your id here
+  local admins = {175636120}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
